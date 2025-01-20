be7962a3d37f0a0cd5fec0ff7c66a893412ce8ee
Michael Swain <michaelswain@tommy.com>
2025-01-20 10:34:25 +0100

    handle pre-selected size for v1

--- TH/v1/index.js
+++ TH/v1/index.js
@@ -43,37 +43,50 @@ const CX1405 = {
         sizeParams.forEach((param, idx) => {
             CX1405.sizeParams.push({});
 
+            const preselectedSize = param.querySelector('[class*="ProductSize_SizeButton"][class*="ProductSize_SizeSelected"]') || false;
+            if (preselectedSize) {
+                CX1405.setSelectedSizes(preselectedSize, idx);
+            }
+
             const config = { attributes: true, childList: true, subtree: true, attributeOldValue: true };
 
             const callback = (mutationList, observer) => {
-            for (const mutation of mutationList) {
-                
-                if (mutation.type === "attributes" && mutation.target.classList.value.includes('SizeButton')) {
-                    CX1405.setSelectedSizes(mutation, idx);
-                    CX1405.renderButtonText();
+                for (const mutation of mutationList) {
                     
-                    // mobile sticky A2B button
-                    if (document.querySelector('[data-testid*="stickyAddToBagButton"] [class*="StickyAddToBag_AddToBagButtonText"]')) {
+                    if (mutation.type === "attributes" && mutation.target.classList.value.includes('SizeButton')) {
+                        CX1405.setSelectedSizes(mutation.target, idx, mutation.oldValue);
                         CX1405.renderButtonText();
+                        
+                        // mobile sticky A2B button
+                        if (document.querySelector('[data-testid*="stickyAddToBagButton"] [class*="StickyAddToBag_AddToBagButtonText"]')) {
+                            CX1405.renderButtonText();
+                        }
                     }
                 }
-            }
             };
 
             const observer = new MutationObserver(callback);
             observer.observe(param, config);
                     });
     },
-    setSelectedSizes: (mutation, idx) => {
-        if (mutation.target.classList.value.includes('SizeSelected')) {
-            CX1405.sizeParams[idx][mutation.target.textContent] = mutation.target.classList.value.includes('ProductSize_IsOos') ? 'outOfStock' : 'inStock';
-        } else if (mutation.oldValue.includes('SizeSelected')) {
-            delete CX1405.sizeParams[idx][mutation.target.textContent];
+    setSelectedSizes: (target, idx, oldValue = false) => {
+        if (target.classList.value.includes('SizeSelected')) {
+            CX1405.sizeParams[idx][target.textContent] = target.classList.value.includes('ProductSize_IsOos') ? 'outOfStock' : 'inStock';
+        } else if (oldValue && oldValue.includes('SizeSelected')) {
+            delete CX1405.sizeParams[idx][target.textContent];
         }
+    },
+    getSelectedSize: () => {
+        let res = '';
+        CX1405.sizeParams.forEach(param => {
+            res += res.length ? ` / ${Object.keys(param)[0]}` : `${Object.keys(param)[0]}`;
+        });
+        return res;
     },
     getButtonText: () => {
         let buttonText = '';
         const selectedSizes = JSON.stringify(CX1405.sizeParams);
+        const userSelection = CX1405.getSelectedSize();
 
         if (!selectedSizes.includes("{}")) {
             const isInStock = CX1405.checkInStock();
