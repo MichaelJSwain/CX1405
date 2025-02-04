f4a97e05fc8407c81c87eb3492c0982bfedbf9a2
Michael Swain <michaelswain@tommy.com>
2025-02-04 10:23:51 +0100

    fix exp ID referenced in custom goal names

--- goals.js
+++ goals.js
@@ -2,15 +2,15 @@ optimizely.utils.waitForElement('[data-testid*="pdpActionButton"]')
     .then(button => {
         button.addEventListener('click', () => {
             if (button.textContent.toLowerCase() === 'select a size') {
-                optimizely.sendAnalyticsEvents(`CX1405 - Clicks on Select size CTA`);
+                optimizely.sendAnalyticsEvents(`CX1045 - Clicks on Select size CTA`);
             }
         });
     });
 optimizely.utils.observeSelector('[data-testid="selectSizeError"]', () => {
-    optimizely.sendAnalyticsEvents("CX1405 - Please select a size message is shown");
+    optimizely.sendAnalyticsEvents("CX1045 - Please select a size message is shown");
 });
 optimizely.utils.waitForElement('[data-testid="pdp-size-guide"]', sizeGuide => {
     sizeGuide.addEventListener('click', () => {
-        optimizely.sendAnalyticsEvents("CX1405 - Clicks on size guide");
+        optimizely.sendAnalyticsEvents("CX1045 - Clicks on size guide");
     });
 });
